14e5f31a9d5f77eac8790d2ae8d8cf8d514dcdaf
SurenSK <kumar.s.suren@gmail.com>
2024-02-05 20:00:08 -0500

    higher batch size test

--- getResp_perf.py
+++ getResp_perf.py
@@ -5,14 +5,13 @@ class Args():
     dataset='yelp_review_full'
     demonstrations_fp="ICV_alt/sentiment_demonstrations.csv"
     alpha=1
-    num_samples=10
+    num_samples=256
     batch_size=32
     truncation_len=512
     in_8bit=True
     model_type='falcon'
     model_size='7b'
     max_length=20
-    num_samples = 64
     dataset_fp = "processed_dataset.jsonl"
     num_alphas = 11
     num_repeats = 3
@@ -38,8 +37,19 @@ alphas = np.linspace(args.a0, args.a1, args.num_alphas)
 indices = np.linspace(0, len(dataset)-1, args.num_samples, dtype=int)
 dataset = dataset.select(indices)
 
+
+
 import time
-text_pipe, sent_pipe = setup_llm_calls(args)
-t0=time.time()
-sents = prompt_to_sent(dataset, args.num_repeats, text_pipe, sent_pipe)
-print(f"Time taken: {time.time()-t0} Samples/sec: {args.num_samples/(time.time()-t0)}")
+def print_perf(args, dataset):
+    text_pipe, sent_pipe = setup_llm_calls(args)
+    t0=time.time()
+    sents = prompt_to_sent(dataset, args.num_repeats, text_pipe, sent_pipe)
+    print(f"Num Samples: {args.num_repeats*args.num_samples} Batch Size: {args.batch_size} Time taken: {time.time()-t0:.2f} Samples/sec: {args.num_repeats*args.num_samples/(time.time()-t0):.2f}")
+
+print_perf(args, dataset)
+args.batch_size = 64
+print_perf(args, dataset)
+args.batch_size = 128
+print_perf(args, dataset)
+args.batch_size = 256
+print_perf(args, dataset)
