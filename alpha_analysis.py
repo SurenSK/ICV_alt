9632c18de6e850cc179155992e40ba9fb12d5742
SurenSK <kumar.s.suren@gmail.com>
2024-02-12 20:46:32 -0500

    batch sz

--- alpha_analysis.py
+++ alpha_analysis.py
@@ -30,7 +30,7 @@ class Args():
     demonstrations_fp="ICV_alt/sentiment_demonstrations.csv"
     alpha=1.0
     num_samples=15
-    batch_size=150 #112
+    batch_size=128 #112
     in_8bit=True #True
     model_type='falcon' #falcon
     model_size='7b' #7b
