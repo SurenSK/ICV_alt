01dd44cb4db98e70af94124df7eb17c5641ea6cd
SurenSK <kumar.s.suren@gmail.com>
2024-02-28 00:02:21 -0500

    classify with assistant force

--- code_vuln_test.py
+++ code_vuln_test.py
@@ -1,12 +1,17 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
+import json
+
 tokenizer = AutoTokenizer.from_pretrained("cognitivecomputations/dolphin-2.2.1-mistral-7b")
 model = AutoModelForCausalLM.from_pretrained("cognitivecomputations/dolphin-2.2.1-mistral-7b", load_in_8bit=True)
+
+with open('code_examples.json', 'r') as f:
+    code_demos = json.load(f)
 
 def llm_bin_classify(query):
     prompt = [
         {"role": "system", "content": "This is a binary classification task. Reply with \"yes\" or \"no\" only, nothing else."},
-        {"role": "user", "content": f"{query}"}]
+        {"role": "user", "content": f"{query}\n<|im_start|>assistant"}]
     model_inputs  = tokenizer.apply_chat_template(prompt, return_tensors="pt", padding=True).to('cuda')
     generated_ids = model.generate(
                 model_inputs,
@@ -20,7 +25,7 @@ def llm_bin_classify(query):
     print(logits.shape)
 
     predicted_token_ids = logits.argmax(dim=-1).tolist()
-    predicted_tokens = tokenizer.decode(predicted_token_ids, skip_special_tokens=True)
+    predicted_tokens = tokenizer.batch_decode(predicted_token_ids, skip_special_tokens=True)
     print(predicted_tokens) 
 
     logits = generated_ids.scores[0][0]
@@ -34,16 +39,6 @@ def llm_bin_classify(query):
 
     return is_vulnerable.item(), confidence.item()
 
-# for code,refusal in code_demos:
-#     print(llm_bin_classify(f"Does the following code contain any vulnerabilities? {code}"))
-# print(llm_bin_classify("Is the sky blue?"))
-messages = [
-    {"role": "user", "content": "What is your favourite condiment?"},
-    {"role": "assistant", "content": "Well, I'm quite partial to a good squeeze of fresh lemon juice. It adds just the right amount of zesty flavour to whatever I'm cooking up in the kitchen!"},
-    {"role": "user", "content": "Do you have mayonnaise recipes?\n<|im_start|>assistant"}
-]
-
-model_inputs = tokenizer.apply_chat_template(messages, return_tensors="pt").to("cuda")
-print(model_inputs)
-generated_ids = model.generate(model_inputs, max_new_tokens=100, do_sample=True)
-print(tokenizer.batch_decode(generated_ids)[0])
+for code,refusal in code_demos:
+    print(llm_bin_classify(f"Does the following code contain any vulnerabilities? {code}"))
+print(llm_bin_classify("Is the sky blue?"))
