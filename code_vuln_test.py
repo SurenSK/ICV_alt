1ce5d687164490ccb3283dddc9dd5fe3989aba5f
SurenSK <kumar.s.suren@gmail.com>
2024-02-27 23:55:07 -0500

    force assistant message

--- code_vuln_test.py
+++ code_vuln_test.py
@@ -40,10 +40,10 @@ def llm_bin_classify(query):
 messages = [
     {"role": "user", "content": "What is your favourite condiment?"},
     {"role": "assistant", "content": "Well, I'm quite partial to a good squeeze of fresh lemon juice. It adds just the right amount of zesty flavour to whatever I'm cooking up in the kitchen!"},
-    {"role": "user", "content": "Do you have mayonnaise recipes?"}
+    {"role": "user", "content": "Do you have mayonnaise recipes?\n<|im_start|>assistant"}
 ]
 
 model_inputs = tokenizer.apply_chat_template(messages, return_tensors="pt").to("cuda")
-
+print(model_inputs)
 generated_ids = model.generate(model_inputs, max_new_tokens=100, do_sample=True)
 print(tokenizer.batch_decode(generated_ids)[0])
