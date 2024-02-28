e4cebbaca55e7c2518a50f1ed9ea79697bc2611a
SurenSK <kumar.s.suren@gmail.com>
2024-02-27 21:51:55 -0500

    reference

--- code_vuln_test.py
+++ code_vuln_test.py
@@ -56,9 +56,9 @@ task_agent.set_seed(args.seed)
 model, tokenizer, text_pipe, _ = setup_llm_calls(args)
 print(model)
 print(model.__dict__)
-if args.model == 'falcon':
+if model == 'falcon':
     args.num_layers = len(model.transformer.h)
-elif args.model == 'dolphin':
+elif model == 'dolphin':
     args.num_layers = len(model.layers)
 args.max_length = text_pipe.tokenizer.model_max_length
 with open('code_examples.json', 'r') as file:
